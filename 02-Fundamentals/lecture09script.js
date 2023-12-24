66646d3268c9896341a12cd0f70b6ae4fd3a688d
b4t54ndw1ch <jagoblacksky@gmail.com>
2023-12-24 16:44:42 -0600

    Moved things around

---
+++ 02-Fundamentals/lecture09script.js
@@ -0,0 +1,10 @@
+console.log('Start of Lecture 9: ');
+
+
+
+
+
+
+console.log('   ');
+console.log('End of Lecture 9: ');
+console.log('   ');
--- 02-Fundamentals/script.js
+++ 02-Fundamentals/script.js
@@ -3,4 +3,9 @@ let js = 'amazing';
 if (js === 'amazing') alert('JavaScript is FUN!');
 
 let mathProblem = 40 + 8 + 23 - 10;
+
 console.log('The result of the math problem is: ' + mathProblem);
+console.log('   ');
+console.log('End of Lecture 8');
+console.log('   ');
+console.log('   ');
