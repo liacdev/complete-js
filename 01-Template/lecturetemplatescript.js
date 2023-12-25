b40f138f5beab03058c465adb4cac68e7248d6d8
b4t54ndw1ch <jagoblacksky@gmail.com>
2023-12-25 17:05:05 -0600

    Prep for lesson 14

---
+++ 01-Template/lecturetemplatescript.js
@@ -0,0 +1,7 @@
+console.log('**********');
+console.log('Lecture 14: Operator Precedence');
+
+console.log('   ');
+console.log('End of Lecture 14');
+console.log('**********');
+console.log('   ');
--- 01-Template/scriptTemplate.js
+++ 01-Template/scriptTemplate.js
@@ -4,3 +4,4 @@ if (js === 'template') alert('This is an HTML template');
 
 let mathProblem = 50 + 38 + 43 - 32;
 console.log('The result of the math problem is: ' + mathProblem);
+console.log('   ');
