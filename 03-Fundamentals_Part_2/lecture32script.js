551e1fe12a86c6cf3731248e703bafee4cb8cc48
b4t54ndw1ch <jagoblacksky@gmail.com>
2023-12-26 20:30:03 -0600

    Setup for Lesson 32

---
+++ 03-Fundamentals_Part_2/lecture32script.js
@@ -0,0 +1,7 @@
+console.log('**********');
+console.log('Lecture 32: Activating Strict Mode');
+
+console.log('   ');
+console.log('End of Lecture 32');
+console.log('**********');
+console.log('   ');
