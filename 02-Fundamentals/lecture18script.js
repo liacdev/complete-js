23179981dc43ee19fbbbdd47f1045366ccf0723f
b4t54ndw1ch <jagoblacksky@gmail.com>
2024-01-01 13:38:53 -0600

    Lecture 18 complete

--- 02-Fundamentals/lecture18script.js
+++ 02-Fundamentals/lecture18script.js
@@ -6,6 +6,47 @@ console.log('Lecture 18: Taking Decisions: if / else Statements');
 /*
 Notes:
 */
+console.log(`   `);
+
+// Is this person old enough to drive?
+const ageSarah = 19;
+const ageSteve = 15;
+const ageToDrive = 18;
+
+console.log(`Sarah is ${ageSarah} years old.`);
+console.log(`The age to drive a car is ${ageToDrive} years old.`);
+
+if (ageSarah >= ageToDrive) {
+    console.log(`Sarah is old enough to drive.`)
+} else {
+    console.log(`Sarah is not old enough to drive.`)
+}
+
+console.log(`   `);
+
+console.log(`Steve is ${ageSteve} years old.`);
+console.log(`The age to drive a car is ${ageToDrive} years old.`);
+
+if (ageSteve >= ageToDrive) {
+    console.log(`Steve is old enough to drive.`)
+} else {
+    console.log(`Steve is not old enough to drive.`)
+}
+
+console.log(`   `);
+
+let century;
+
+const birthYear1 = 1991;
+if (birthYear1 <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(`The century is ${century}`);
+
+
 
 console.log('   ');
 console.log('End of Lecture 18');
