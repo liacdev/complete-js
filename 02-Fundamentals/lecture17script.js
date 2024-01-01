9b80435c1d9a77334853a9985823a6b72416569f
b4t54ndw1ch <jagoblacksky@gmail.com>
2024-01-01 10:33:30 -0600

    Happy New Year!

---
+++ 02-Fundamentals/lecture17script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+console.log('**********');
+console.log('Lecture 17: Strings and Template Literals');
+
+/*
+
+*/
+
+
+
+
+console.log('   ');
+console.log('End of Lecture 17');
+console.log('**********');
+console.log('   ');
